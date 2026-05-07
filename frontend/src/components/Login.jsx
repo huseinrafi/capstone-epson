760454a91eda5bd54862d1beffa4aaf8ffbbd2ff
Ramadhan Cahaya <bimarama2014@gmail.com>
2026-05-07 17:45:24 +0700

    env example dan dynamic API url

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/v1/auth/login', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -28,7 +28,7 @@ export default function Register() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/v1/auth/register', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
